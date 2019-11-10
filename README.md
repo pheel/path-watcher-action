35af51addb57b1af109d3200237126f9f1892232
Philippe Cote-Boucher <philippe@mrbot.ai>
2019-11-10 10:10:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## path-watcher-action
+
+Given a comma-separated list of globs, returns whether the head commit that triggered the action involves a change in any of the input paths.
+
+Usage:
+
+```
+on: [push]
+
+jobs:
+  job:
+    runs-on: ubuntu-latest
+    steps:
+      - id: modified
+        uses: pheel/path-watcher-action@v1
+        with:
+          github_token: ${{ secrets.GITHUB_TOKEN }} # no need to set this secret, it is always available!
+          paths: 'dir1/**/*,dir2/**/*'
+      - if: steps.modified.outputs.modified
+        run: echo "Hey some change happened in one of your watched paths!"
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const Octokit = require("@octokit/rest");
+const minimatch = require('minimatch');
+
+try {
+  const ghToken = core.getInput('github_token');
+  const octokit = new Octokit(ghToken ? { auth: ghToken } : {});
+  const paths = core.getInput('paths').split(',');
+  const commit_sha = github.context.payload.head_commit.id;
+  const [owner, repo] = github.context.payload.repository.full_name.split('/');
+  octokit.git.getCommit({ owner, repo, commit_sha })
+    .then(({ data: { files } }, err) => {
+      if (err) throw new Error(err);
+      if (Array.isArray(files)) {
+        const modifiedPaths = files.map(f => f.filename);
+        const modified = paths.some(p => minimatch.match(modifiedPaths, p).length);
+        core.setOutput('modified', modified);
+      } else {
+        core.setOutput('modified', false);
+      }
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
