965f44800e47fcce9726fecd78e59cf1f3301834
Dhi Aurrahman <dio@rockybars.com>
2022-03-01 14:40:23 +0700

    Catch the error
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- index.js
+++ index.js
@@ -3,34 +3,33 @@ const github = require('@actions/github');
 const Octokit = require("@octokit/rest");
 const minimatch = require('minimatch');
 
-try {
+async(() => {
   const ghToken = core.getInput('github_token');
   const octokit = new Octokit(ghToken ? { auth: ghToken } : {});
   const paths = core.getInput('paths').split(',');
-  const returnFiles = core.getInput('return_files') === 'true';
+
   // When using pull_request, the context payload.head_commit is undefined. But we have after instead.
   const ref = github.context.payload.head_commit ?
     github.context.payload.head_commit.id : github.context.payload.after;
+
+  if (!ref) {
+    core.setOutput('modified', true);
+    return
+  }
+
   const [owner, repo] = github.context.payload.repository.full_name.split('/');
-  octokit.repos.getCommit({ owner, repo, ref })
-    .then(({ data: { files } }, err) => {
-      if (err) throw new Error(err);
-      if (Array.isArray(files)) {
-        const modifiedPaths = files.map(f => f.filename);
-        if(!returnFiles) {
-          const modified = paths.some(p => minimatch.match(modifiedPaths, p).length);
-          core.setOutput('modified', modified);
-        } else {
-          const modified_files = paths.flatMap(p => minimatch.match(modifiedPaths, p));
-          core.setOutput('modified', modified_files.length > 0);
-          core.setOutput('modified_files', modified_files);
-        }
-      } else {
-        core.setOutput('modified', false);
-      }
-  });
-} catch (error) {
-  core.setFailed(error.message);
-  // When we error, we set modified to true to make sure the tests run.
-  core.setOutput('modified', true);
-}
+  const { data: { files }, err } = await octokit.repos.getCommit({ owner, repo, ref })
+  if (err) {
+    console.error(err);
+    core.setOutput('modified', true);
+    return
+  }
+
+  if (Array.isArray(files)) {
+    const modified = paths.some(p => minimatch.match(modifiedPaths, p).length);
+    core.setOutput('modified', modified);
+    return
+  }
+
+  core.setOutput('modified', false);
+})();
