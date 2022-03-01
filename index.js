6ee60a16ad3e07796f7640d65b41ab57e7e89644
Dhi Aurrahman <dio@rockybars.com>
2022-03-01 16:12:59 +0700

    Print all info
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const minimatch = require('minimatch');
   const ghToken = core.getInput('github_token');
   const octokit = new Octokit(ghToken ? { auth: ghToken } : {});
   const paths = core.getInput('paths').split(',');
+  core.info(JSON.stringify(paths, null, 2));
 
   // When using pull_request, the context payload.head_commit is undefined. But we have after instead.
   let ref;
@@ -21,6 +22,7 @@ const minimatch = require('minimatch');
   core.info(JSON.stringify(github.context.payload, null, 2));
 
   if (!ref) {
+    core.info('failed to get ref');
     core.setOutput('modified', true);
     return
   }
@@ -28,6 +30,7 @@ const minimatch = require('minimatch');
   const [owner, repo] = github.context.payload.repository.full_name.split('/');
   const { data: { files }, err } = await octokit.repos.getCommit({ owner, repo, ref })
   if (err) {
+    core.info('got error')
     core.error(err);
     core.setOutput('modified', true);
     return
@@ -35,6 +38,7 @@ const minimatch = require('minimatch');
 
   if (Array.isArray(files)) {
     const modifiedPaths = files.map(f => f.filename);
+    core.info(JSON.stringify(modifiedPaths, null, 2));
     const modified = paths.some(p => minimatch.match(modifiedPaths, p).length);
     core.setOutput('modified', modified);
     return
